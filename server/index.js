912042247000d46b3fd8f51b9011be17ea2123cd
virenishere <virender288@gmail.com>
2025-07-17 10:52:30 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -3,27 +3,31 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const dbConnection = require("./configs/dbConnection");
 const templateRoutes = require("./routes/templeRoutes");
-const PORT = process.env.PORT || 8000;
 
-const app = express();
 dotenv.config();
 
-// Middleware
-app.use(cors());
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// CORS config
+app.use(cors({
+  origin: ["http://localhost:5173", "https://web-template-editor.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
-// Connect to DB
+// DB Connection
 dbConnection();
 
 // Routes
 app.use("/api/templates", templateRoutes);
 
-app.get("/",(req,res)=>{
-    res.json({
-        message: "server is running..."
-    })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "server is running..." });
+});
 
-app.listen(PORT , ()=>{
-    console.log(`server is running at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
