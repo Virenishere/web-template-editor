13f87644c56061f6611038999d99df4894164b78
virenishere <virender288@gmail.com>
2025-07-17 10:57:18 +0530

    added cors to test

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+//added env test 
 const baseURL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:8000"; 
 
 const instance = axios.create({
   baseURL,
